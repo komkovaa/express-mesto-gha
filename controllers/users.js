dc84c415e241821b3550851b8d1eedd699f634b0
komkova <komkova.ar@gmail.com>
2022-10-27 16:03:44 +0300

    feat: роуты для обновления данных пользователя и аватара

--- controllers/users.js
+++ controllers/users.js
@@ -19,3 +19,33 @@ module.exports.getUserId = (req, res) => {
     .then((userId) => res.send({ data: userId }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
+
+module.exports.updateProfileInfo = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true, // обработчик then получит на вход обновлённую запись
+      runValidators: true, // данные будут валидированы перед изменением
+      // upsert: true, // если пользователь не найден, он будет создан
+    },
+  )
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true, // обработчик then получит на вход обновлённую запись
+      runValidators: true, // данные будут валидированы перед изменением
+      // upsert: true, // если пользователь не найден, он будет создан
+    },
+  )
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 const userRouter = require('express').Router();
-const { getUsers, createUser, getUserId } = require('../controllers/users');
+const { getUsers, createUser, getUserId, updateProfileInfo, updateAvatar } = require('../controllers/users');
 
 userRouter.get('/', getUsers);
 
 userRouter.get('/:userId', getUserId);
 
 userRouter.post('/', createUser);
+
+userRouter.patch('/me', updateProfileInfo);
+
+userRouter.patch('/me/avatar', updateAvatar);
 
 module.exports = userRouter;
