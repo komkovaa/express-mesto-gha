3e6e7ec0bc4cda38231e125a7c125bdddda5db46
komkova <komkova.ar@gmail.com>
2022-10-31 11:43:15 +0300

    обработка ошибок создания Card

--- controllers/cards.js
+++ controllers/cards.js
@@ -25,7 +25,7 @@ module.exports.getCards = (req, res) => {
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link })
+  Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
