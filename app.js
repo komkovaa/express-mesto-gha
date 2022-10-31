7e9fb4eb6133a7b0a7831f886ce49abcfbc8c567
komkova <komkova.ar@gmail.com>
2022-10-31 12:53:38 +0300

    некорректный путь

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ app.use((req, res, next) => {
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
+app.use((req, res) => {
+  res.status(404)
+    .send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
