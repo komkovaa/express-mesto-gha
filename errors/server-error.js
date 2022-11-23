8a5c68eb1652f7ea105112c64284b53d8f01e4af
komkova <komkova.ar@gmail.com>
2022-11-23 09:58:59 +0300

    fix:server-error

--- errors/server-error.js
+++ errors/server-error.js
@@ -2,8 +2,8 @@ const http2 = require('node:http2');
 const HTTPError = require('./http-error');
 
 class ServerError extends HTTPError {
-  constructor(message = '') {
-    super(`Неизвестная ошибка. ${message}`, http2.constants.HTTP_STATUS_SERVICE_UNAVAILABLE);
+  constructor(message) {
+    super(message, http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR);
   }
 }
 
