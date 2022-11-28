ec645ff8b5552ace3c0fe4eaf62cfb2869fb69b6
komkova <komkova.ar@gmail.com>
2022-11-28 15:51:54 +0300

    логирование запросов и ошибок

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const http2 = require('node:http2');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(requestLogger);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +20,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use((err, req, res, next) => {
